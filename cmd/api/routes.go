db052c7b4054b390de2fd28e0db6e195cf7273e7
gurupatoh <patrickn0023@gmail.com>
2025-09-01 10:32:57 -0400

    cors added

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -5,11 +5,22 @@ import (
     "github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/gin-contrib/cors"
+	"time"
 )
 
 func (app *application) routes() http.Handler {
 
     g := gin.Default()
+	config := cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}
+	g.Use(cors.New(config))
+
     v1 := g.Group("/api/v1")
     {
         v1.GET("/events", app.getAllEvents)
@@ -17,8 +28,8 @@ func (app *application) routes() http.Handler {
         v1.GET("/events/:id/attendees", app.getAttendeesForEvent)
         v1.GET("/attendees/:id/events", app.getEventsByAttendee)
 
-        v1.POST("/register", app.registerUser)
-        v1.POST("/login", app.login)
+        v1.POST("/auth/register",app.registerUser)
+        v1.POST("auth/login",app.login)
     }
 
     authGroup := v1.Group("/")
